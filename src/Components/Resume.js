a49b494cdc319cb3c21d58876f9cb447a494cf0c
1brahimsaid <ibrahim.said95@gmail.com>
2021-07-03 22:15:30 -0400

    h3 maybe

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -49,13 +49,13 @@ class Resume extends Component {
       const className = "bar-expand " + skills.name.toLowerCase();
       const width = skills.level;
       return (
-        <ul key={skills.name}>
+        <h3 key={skills.name}>
           <span style={{ width, backgroundColor }} className={className}></span>
           <em>{skills.name}</em>
           <p className="info">
           <p>{skills.description}</p>
           </p>
-        </ul>
+        </h3>
       );
     });
 
