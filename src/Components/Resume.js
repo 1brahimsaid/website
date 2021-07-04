11f3073d2304a775ce1696b3a388e5dfbf39ad0b
1brahimsaid <ibrahim.said95@gmail.com>
2021-07-03 21:57:40 -0400

    trash

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -49,13 +49,13 @@ class Resume extends Component {
       const className = "bar-expand " + skills.name.toLowerCase();
       const width = skills.level;
       return (
-        <div key={skills.name}>
+        <li key={skills.name}>
           <span style={{ width, backgroundColor }} className={className}></span>
           <em>{skills.name}</em>
           <p className="info">
           <p>{skills.description}</p>
           </p>
-        </div>
+        </li>
       );
     });
 
