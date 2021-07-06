20317fe147ebb9e0c35f1dd5ba9b9d7a07e5ad26
1brahimsaid <ibrahim.said95@gmail.com>
2021-07-06 17:15:10 -0400

    . was a miss

--- src/Components/About.js
+++ src/Components/About.js
@@ -30,7 +30,7 @@ class About extends Component {
               <p>{bio}</p>
               <div className="row">
                 <div className="columns contact-details">
-                  <h2>Contact.</h2>
+                  <h2>Contact</h2>
                   <p className="address">
                     <span>{name}</span>
                     <br />
