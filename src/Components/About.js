508d4b227b0ae25f2b754f6c2cd6b81e5ba2e047
1brahimsaid <ibrahim.said95@gmail.com>
2021-07-06 22:59:06 -0400

    download to file

--- src/Components/About.js
+++ src/Components/About.js
@@ -48,7 +48,7 @@ class About extends Component {
                 <div className="columns download">
                   <p>
                     <a href={resumeDownload} className="button">
-                      <i className="fa fa-download"></i>View Resume
+                      <i className="fa fa-file"></i>View Resume
                     </a>
                   </p>
                 </div>
